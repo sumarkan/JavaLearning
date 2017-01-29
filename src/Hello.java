5a7dc52ee069a90a2f19a8160de0f8c303317317
santosh <santosh26192@gmail.com>
2017-01-29 13:07:59 -0800

    Hello File

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Created by santosh on 1/29/2017.
+ */
+
+import static java.lang.System.out;
+
+public class Hello {
+    public static void main(String[] args) {
+        out.println("Hi");
+    }
+}
